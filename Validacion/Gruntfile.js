ba256891d35ac00f97598cefbd64e281a42a0bc7
AlanFox <>
2016-09-22 12:02:12 +0200

    index.html added

---
+++ Validacion/Gruntfile.js
@@ -0,0 +1,9 @@
+/* 
+ * DAW2
+ * Alan Fox Echezarreta
+ */
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ Validacion/gulpfile.js
@@ -0,0 +1,10 @@
+/* 
+ * DAW2
+ * Alan Fox Echezarreta
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
