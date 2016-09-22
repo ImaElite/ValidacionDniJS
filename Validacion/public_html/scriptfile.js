c701ce8d34c39b3e8d0646522d31af7ee86882e3
AlanFox <usuario@192.168.36.1>
2016-09-22 12:08:29 +0200

    scriptfile.js added arrays created

---
+++ Validacion/public_html/scriptfile.js
@@ -0,0 +1,6 @@
+/* 
+ * DAW2
+ * Alan Fox Echezarreta
+ */
+var colores = ["#DF0101", "#DF0101", "#DF0101", "#DF0101", "#DF7401","#D7DF01", "#31B404", "#01DFD7", "#0101DF", "#FF00BF"];
+var notas = {"DIW":0, "DAW":0, "PHP":0, "JVS":0};
