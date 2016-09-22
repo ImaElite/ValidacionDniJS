e5e7ec41e6140426bda680c51a4ea6277321d100
AlanFox <usuario@192.168.36.1>
2016-09-22 12:34:55 +0200

    more javascript

--- Validacion/public_html/scriptfile.js
+++ Validacion/public_html/scriptfile.js
@@ -7,3 +7,8 @@ var dni;
 var colores = ["#DF0101", "#DF0101", "#DF0101", "#DF0101", "#DF7401", "#D7DF01", "#31B404", "#01DFD7", "#0101DF", "#FF00BF"];
 var notas = {"DIW": 0, "DAW": 0, "PHP": 0, "JVS": 0};
 nombre = prompt("nombre?");
+dni = prompt("dni?");
+notas["DIW"]=prompt("DIW?");
+notas["DAW"]=prompt("DAW?");
+notas["PHP"]=prompt("PHP?");
+notas["JVS"]=prompt("JVS?");
