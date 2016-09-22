db6fba17b7b7703f9e08aaf4a69afad318297307
AlanFox <usuario@192.168.36.1>
2016-09-22 13:02:50 +0200

    more javascript

--- Validacion/public_html/scriptfile.js
+++ Validacion/public_html/scriptfile.js
@@ -8,13 +8,13 @@ var colores = ["#DF0101", "#DF0101", "#DF0101", "#DF0101", "#DF7401", "#D7DF01",
 var notas = [["DIW", 0], ["DAW", 0], ["PHP", 0], ["JVS", 0]];
 nombre = prompt("nombre?");
 dni = prompt("dni?");
-notas[0]=prompt("DIW?");
-notas[2]=prompt("DAW?");
-notas[3]=prompt("PHP?");
-notas[4]=prompt("JVS?");
-var htmlString="<table>\n<tr>\n<th>Nombre Asignatura</th>\n<th>Nota</th>\n<\tr>\n";
-for (var nota in notas){
-    htmlString.concat("<tr><td>", nota[0], "</td>\n<td>", nota[1], "</td></tr>\n");
+notas[0][1]=prompt("DIW?");
+notas[1][1]=prompt("DAW?");
+notas[2][1]=prompt("PHP?");
+notas[3][1]=prompt("JVS?");
+document.write("<table>\n<tr>\n<th>Nombre Asignatura</th>\n<th>Nota</th>\n<\tr>\n");
+var nota;
+for (nota in notas){
+    document.write("<tr><td>", nota[0], "</td>\n<td>", nota[1], "</td></tr>\n");
 }
-htmlString.concat("</table>");
-document.write();
+document.write("</table>");
