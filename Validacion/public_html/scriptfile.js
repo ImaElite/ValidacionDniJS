ec3845e141e62c60dbbf4bffdfe905d9e559017a
AlanFox <usuario@192.168.36.1>
2016-09-22 13:35:13 +0200

    apsdtufyvuvgfyuoyg

--- Validacion/public_html/scriptfile.js
+++ Validacion/public_html/scriptfile.js
@@ -4,7 +4,7 @@
  */
 var nombre;
 var dni;
-var colores = ["#DF0101", "#DF0101", "#DF0101", "#DF0101", "#DF7401", "#D7DF01", "#31B404", "#01DFD7", "#0101DF", "#FF00BF"];
+var colores = ["red", "red", "red", "red", "red", "orange", "yellow", "cyan", "green", "gray", "purple"];
 var notas = [["DIW", 0], ["DAW", 0], ["PHP", 0], ["JVS", 0]];
 nombre = prompt("nombre?");
 do {
@@ -19,29 +19,30 @@ notas[1][1] = prompt("DAW?");
 notas[2][1] = prompt("PHP?");
 notas[3][1] = prompt("JVS?");
 document.write("<table>\n<tr>\n<th>Asignatura</th>\n<th>Nota</th>\n<\tr>\n");
-for (i = 0; len = notas.length; i++ < len) {
-    document.write("<tr><td>", notas[i][0], "</td>\n<td bgcolor=\"", colores[notas[i][1]], ";\">", notas[i][1], "</td></tr>\n");
+for (i = 0; i < notas.length; i++) {
+    alert(colores[notas[i][1]]);
+    document.write("<tr><td>", notas[i][0], '</td>\n<td bgcolor="', colores[notas[i][1]], ";\">", notas[i][1], "</td></tr>\n");
 }
 document.write("</table>");
 
 
 
 function nif(dni) {
-    var numero
-    var letr
-    var letra
-    var expresion_regular_dni
+    var numero;
+    var letr;
+    var letra;
+    var expresion_regular_dni;
     var correcto = false;
 
     expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
 
-    if (expresion_regular_dni.test(dni) == true) {
+    if (expresion_regular_dni.test(dni) === true) {
         numero = dni.substr(0, dni.length - 1);
         letr = dni.substr(dni.length - 1, 1);
         numero = numero % 23;
         letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
         letra = letra.substring(numero, numero + 1);
-        if (letra != letr.toUpperCase()) {
+        if (letra !== letr.toUpperCase()) {
             alert('Dni erroneo, la letra del NIF no se corresponde');
         } else {
             alert('Dni correcto');
